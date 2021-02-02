28958ade08a86e4682e356e8b5f18c0fd1d935b4
Kareem Ali <kareem.ali.rahman@gmail.com>
2021-02-02 20:31:22 +0200

    fix files paths

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,15 @@
 import express from "express"; 
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 
 const app = express();
 const startServer = async () => {
     try {
         await createConnection().then(connection =>{
+          console.log("connected to db");
         });
     }
     catch (err){
         console.log(err);
-        await new Promise(res => setTimeout(res, 5000));
     }
     app.get('/', (req, res, next) => {      
       res.send('Hello world')
