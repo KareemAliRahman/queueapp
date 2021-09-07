1eac38a7ed6d75fd91e5f487005f5cc92b3053fa
Kareem Ali <kareem.ali.rahman@gmail.com>
2021-09-07 18:05:25 +0200

    small change

--- server/db/entity/Queue.ts
+++ server/db/entity/Queue.ts
@@ -1,39 +1,47 @@
-import { Length } from "class-validator";
-import { BaseEntity, Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn, Timestamp } from "typeorm";
-import { User } from "./User";
+import { Length } from 'class-validator';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Timestamp,
+} from 'typeorm';
+import { User } from './User';
 
 @Entity()
 export class Queue extends BaseEntity {
-
-  @PrimaryGeneratedColumn("rowid", { name: 'id' })
+  @PrimaryGeneratedColumn('rowid', { name: 'id' })
   id: string;
 
   //qrcode dataurl
-  @Column({nullable: true})
+  @Column({ nullable: true })
   qrcode: string;
 
-  @Column({ length: 100 , nullable: false })
+  @Column({ length: 100, nullable: false })
   @Length(1, 100)
   name: string;
 
-  @Column({ length: 100 , nullable: true })
+  @Column({ length: 100, nullable: true })
   @Length(1, 100)
   organization: string;
 
-  @Column({ length: 300 , nullable: true })
+  @Column({ length: 300, nullable: true })
   @Length(1, 300)
   description: string;
 
-  @Column({nullable: false})
-  address: string
+  @Column({ nullable: false })
+  address: string;
 
-  @Column("timestamp with time zone", {nullable: false})
-  startsAt: Date
+  @Column('timestamp with time zone', { nullable: false })
+  startsAt: Date;
 
-  @ManyToOne(type => User, admin => admin.queues)
+  @ManyToOne((type) => User, (admin) => admin.queues)
   admin: User;
 
-  @ManyToMany(type => User, member => member.enlistedQueues)
+  @ManyToMany((type) => User, (member) => member.enlistedQueues)
   @JoinTable()
   members: User[];
 }
